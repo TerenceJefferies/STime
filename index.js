32458cc80179d36d0c0a11992741de3ab9febc09
Terence Jefferies <terencethedev@gmail.com>
2020-06-27 18:47:48 +0100

    Convert main into source file

--- index.js
+++
@@ -1,4 +0,0 @@
-const STime = require('./dist/STime').default;
-const TimeFactory = require('./dist/TimeFactory').default;
-
-module.exports = new STime(new TimeFactory());
---
+++ src/Main.ts
@@ -0,0 +1,4 @@
+import STime from './STime';
+import TimeFactory from './TimeFactory';
+
+module.exports = new STime(new TimeFactory());
