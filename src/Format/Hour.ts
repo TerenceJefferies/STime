4a9386e9da0b3e6cddbd9642c75529b8d6c4e511
Terence Jefferies <terencethedev@gmail.com>
2020-06-29 22:15:44 +0100

    Add twelve hour formatting

--- src/Format/Hour.ts
+++ src/Format/Hour.ts
@@ -9,14 +9,50 @@ class Hour extends Format {
    * @inheritdoc
    */
   format(time: Formattable, format: string): string {
+    let formatted = this.applyTwentyFourHourFormatting(time, format);
+    formatted = this.applyTwelveHourFormatting(time, formatted);
+    return formatted;
+  }
+
+  /**
+   * Apply twenty four hour formatting to a format
+   * @param {Formattable} time Formattable entity to format
+   * @param {string} format Format to use
+   * @return {string} Formatted entity
+   */
+  private applyTwentyFourHourFormatting(time: Formattable, format: string): string {
     let formatted = format.replace(
-        /H{2}/g,
-        this.formatNumber(time.getHours(), true),
+      /H{2}/g,
+      this.formatNumber(time.getHours(), true),
     );
     formatted = formatted.replace(
         /H{1}/g,
         this.formatNumber(time.getHours(), false),
     );
+    return formatted;
+  }
+
+  /**
+   * Apply twelve hour formatting to a format
+   * @param {Formattable} time Format an hour in twelve hour format
+   * @param {string} format Format to use
+   * @return {string} Formatted format
+   */
+  private applyTwelveHourFormatting(time: Formattable, format: string): string {
+    let hour = time.getHours();
+    if (hour > 12) {
+      hour = (hour - 12);
+    }
+
+    let formatted = format.replace(
+      /h{2}/g,
+      this.formatNumber(hour, true)
+    );
+    formatted = formatted.replace(
+      /h{1}/g,
+      this.formatNumber(hour, false)
+    );
+
     return formatted;
   }
 
--- test/Format/Hour.test.ts
+++ test/Format/Hour.test.ts
@@ -47,4 +47,50 @@ describe('Hour', () => {
       expect(days).toEqual(hour);
     }   
   });
+
+  it('Should return 3 from a h format', () => {
+    const time = new Time(new Date('06-09-20 15:00:00'));
+    const formatted = time.format('h');
+  
+    expect(formatted).toEqual('3');
+  });
+
+  it('Should return 03 from a hh format', () => {
+    const time = new Time(new Date('06-09-20 15:00:00'));
+    const formatted = time.format('hh');
+  
+    expect(formatted).toEqual('03');
+  });
+});
+
+describe.each([
+  ['06-09-20 00:00:00', 'h', '0'],
+  ['06-09-20 01:00:00', 'h', '1'],
+  ['06-09-20 02:00:00', 'h', '2'],
+  ['06-09-20 03:00:00', 'h', '3'],
+  ['06-09-20 04:00:00', 'h', '4'],
+  ['06-09-20 05:00:00', 'h', '5'],
+  ['06-09-20 06:00:00', 'h', '6'],
+  ['06-09-20 07:00:00', 'h', '7'],
+  ['06-09-20 08:00:00', 'h', '8'],
+  ['06-09-20 09:00:00', 'h', '9'],
+  ['06-09-20 10:00:00', 'h', '10'],
+  ['06-09-20 11:00:00', 'h', '11'],
+  ['06-09-20 12:00:00', 'h', '12'],
+  ['06-09-20 13:00:00', 'h', '1'],
+  ['06-09-20 14:00:00', 'h', '2'],
+  ['06-09-20 15:00:00', 'h', '3'],
+  ['06-09-20 16:00:00', 'h', '4'],
+  ['06-09-20 17:00:00', 'h', '5'],
+  ['06-09-20 18:00:00', 'h', '6'],
+  ['06-09-20 19:00:00', 'h', '7'],
+  ['06-09-20 20:00:00', 'h', '8'],
+  ['06-09-20 21:00:00', 'h', '9'],
+  ['06-09-20 22:00:00', 'h', '10'],
+  ['06-09-20 23:59:59', 'h', '11'],
+])('Should return the correct format (%s, %s, %s)', (date, format, expected) =>  {
+  const time = new Time(new Date(date));
+  const formatted = time.format(format);
+
+  expect(formatted).toEqual(expected);
 });
