72181c9cbc2eff54c18573fbb332312a056f8dbc
Terence Jefferies <terencethedev@gmail.com>
2020-07-08 20:51:30 +0100

    Fix broken TimeFactory test
    Due to the privatisation of the `root` property on the time class, old test cases that compared the native date in time to known fixtures began to fail. This commit fixes that error.

--- test/TimeFactory.test.ts
+++ test/TimeFactory.test.ts
@@ -8,19 +8,10 @@ describe('TimeFactory', () => {
       const time = factory.create();
 
       expect(
-        time.root.getDay() === fixture.getDay()
-        && time.root.getMonth() === fixture.getMonth()
-        && time.root.getFullYear() === fixture.getFullYear()
+        time.getDay() === fixture.getDate()
+        && time.getMonth() === (fixture.getMonth() + 1)
+        && time.getYear() === fixture.getFullYear()
       ).toBeTruthy();
-    });
-
-    it('Should return a time with the matching native date', () => {
-      const factory = new TimeFactory();
-      const fixture = new Date();
-      
-      const time = factory.fromDate(fixture);
-
-      expect(time.root).toStrictEqual(fixture);
     });
 
     it('Should return a date for yesterday', () => {
