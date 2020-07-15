10ea08d54b2866e2a9cb1542796cfd97bf8b84fd
Terence Jefferies <terencethedev@gmail.com>
2020-07-15 20:08:52 +0100

    Convert year test to use format file

--- test/Format/Year.test.ts
+++ test/Format/Year.test.ts
@@ -1,19 +1,20 @@
 import Time from '../../src/Time';
 import Year from '../../src/Format/Year';
+import Formats from '../../src/Format/Formats';
 
 describe('Year', () => {
   it('Should return a four digit year', () => {
     const time = new Time(new Date('01-01-2020 00:00:00'));
     const format = new Year();
 
-    expect(format.format(time, ':fullYear:')).toEqual('2020');
+    expect(format.format(time, Formats.FOUR_DIGIT_YEAR)).toEqual('2020');
   });
 
   it('Should return a two digit year', () => {
     const time = new Time(new Date('01-01-2019 00:00:00'));
     const format = new Year();
 
-    expect(format.format(time, ':shortYear:')).toEqual('19');
+    expect(format.format(time, Formats.TWO_DIGIT_YEAR)).toEqual('19');
   });
 
   it('Should return the year number from YYYY fromat', () => {
